efb3847a082567af57f40a41d322c6922934122f
Hamid Raza <contact@hamidraza.com>
2014-08-21 01:05:58 +0530

    updated script

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,8 +1,22 @@
 (function($){
-  $('.hcountdown').hCountdown({
-    speed: 100,
-    onComplete: function(){
-      $('button.start-countDown').prop('disabled', false);
-    }
+  function playCountdown(){
+    $('.hcountdown').hCountdown({
+      beforeStart: function(){
+        $('button.start-countDown')
+          .prop('disabled', true)
+          .text('running');
+      },
+      speed: 100,
+      onComplete: function(){
+        $('button.start-countDown')
+          .prop('disabled', false)
+          .text('reStart');
+      }
+    });
+  }
+  playCountdown();
+  $('.hcountdown').on('click', function(e){
+    e.preventDefault();
+    playCountdown();
   });
 }(jQuery));
